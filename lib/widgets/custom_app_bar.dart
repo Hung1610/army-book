c8989b2ab2914cc6a7b6f6fd2cab35e3c846d870
Hung Do <phuochung008@gmail.com>
2021-12-03 16:24:49 +0700

    update app icon

--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -22,7 +22,21 @@ class CustomAppBar extends StatelessWidget {
             WindowTitleBarBox(
               child: Row(
                 children: [
-                  Expanded(child: MoveWindow()),
+                  Expanded(
+                      child: MoveWindow(
+                          child: Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xff2B3138),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(0, 3), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                  ))),
                   WindowButtons(),
                 ],
               ),
